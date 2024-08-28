c03de1e15bd64267c3f6d6d3a1a0f5b345a0de58
adyfp24 <adyfp24@gmail.com>
2024-08-28 14:17:32 +0700

    feat: product endpoint

--- server/controllers/product-controller.js
+++ server/controllers/product-controller.js
@@ -1,4 +1,6 @@
 const productService = require('../services/product-service');
+const { successResponse, clientErrorResponse, errorResponse, 
+        createdResponse, notFoundResponse } = require('../middlewares/response');
 
 const getAllProduct = (req, res) => {
     try {
@@ -19,6 +21,107 @@ const getAllProduct = (req, res) => {
     }
 }
 
+
+const createProduct = async (req, res) => {
+    try {
+        const {
+            nama,
+            stok,
+            harga,
+            jenis_produk
+        } = req.body;
+
+        const product = {
+            nama,
+            stok,
+            harga,
+            jenisProdukId: jenis_produk
+        }
+        const newProduct = await productService.createProduct(product);
+        if (newProduct) {
+            return createdResponse(res, newProduct, "produk berhasil ditambahkan");
+        } else {
+            return clientErrorResponse(res, "produk gagal ditambahkan")
+        }
+    } catch (error) {
+        return errorResponse(res, error);
+    }
+}
+
+// const getAllProduct = async (req, res) => {
+//     try {
+//         const allProduct = await productService.getAllProduct();
+//         if (allProduct != 0) {
+//             return successResponse(res, allProduct, "data seluruh produk berhasil didapat");
+//         } else {
+//             return notFoundResponse(res, "data produk tidak tersedia")
+//         }
+//     } catch (error) {
+//         return errorResponse(res, error);
+//     }
+// }
+
+const getProductById = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const product = await productService.getProductById(productId);
+        if(product != 0){
+            return successResponse(res, product, "data produk dengan id tersebut tersedia")
+        }else{
+            return notFoundResponse(res, "data produk dengan id tersebut tidak tersedia");
+        }
+    } catch (error) {
+        return errorResponse(res, error);
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const deletedProduct = await productService.deleteProduct(productId);
+        if(deletedProduct){
+            return successResponse(res, "data produk berhasil dihapus")
+        }else{
+            return notFoundResponse(res, "data produk gagal dihapus karena id tidak valid");
+        };
+    } catch (error) {
+        return errorResponse(res, error);
+    }
+}
+
+const updateProduct = async (req, res) => {
+    try {
+        const {
+            nama,
+            stok,
+            harga,
+            jenis_produk
+        } = req.body;
+
+        const product = {
+            nama,
+            stok,
+            harga,
+            jenisProdukId: jenis_produk
+        }
+        const productId = req.params.id;
+        const updatedProduct = await productService.updateProduct(productId, product);
+        if(updatedProduct){
+            return successResponse(res, "data produk berhasil diperbarui")
+        }else{
+            return notFoundResponse(res, "data produk gagal diperbarui karena id tidak valid");
+        };
+    } catch (error) {
+        return errorResponse(res, error);
+    }
+}
+
 module.exports = {
-    getAllProduct
-};
+    createProduct,
+    getAllProduct,
+    getProductById,
+    deleteProduct,
+    updateProduct
+}
+
+
--- server/routes/product-route.js
+++ server/routes/product-route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product-controller');
+const { validate } = require('../middlewares/validator');
+const productValidation = require('../validations/product-validation');
 
 router.get('/product', productController.getAllProduct);
-// router.get('/product/:id', productController.getProductById);
-// router.post('/product', productController.createProduct);
-// router.put('/product/:id', productController.createProduct);
-// router.delete('/product/:id', productController.createProduct);
+router.get('/product/:id', productController.getProductById);
+router.post('/product', validate(productValidation.createProduct), productController.createProduct);
+router.delete('/product/:id', productController.deleteProduct);
+router.put('/product/:id', validate(productValidation.updateProduct), productController.updateProduct);
 
 module.exports = router;
