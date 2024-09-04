06bea2f9cdbad27785c43674e7846e3494edfade
adyfp24 <adyfp24@gmail.com>
2024-09-04 13:58:24 +0700

    feat: nav and sidebar components

--- src/components/fragments/LoginForm.jsx
+++ src/components/fragments/LoginForm.jsx
@@ -52,7 +52,7 @@ function LoginForm() {
                                 </div>
                                 <Link to='/dashboard' >
                                     <button type="submit"
-                                        className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
+                                        className="w-full mt-4 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
                                         in</button>
                                 </Link>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/layouts/Navbar'
+import Sidebar from '../components/layouts/Sidebar'
 
 function LandingPage() {
     return (
         <>
             <Navbar />
-            <div>LandingPage</div>
+            <Sidebar />
         </>
     )
 }
