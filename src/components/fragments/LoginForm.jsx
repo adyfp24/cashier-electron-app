dc6ef22abf3ae2e6f095b0d11230d11e936b1704
adyfp24 <adyfp24@gmail.com>
2024-12-13 20:09:43 +0700

    [FIX] vurnebility of depedencies

--- src/components/fragments/LoginForm.jsx
+++ src/components/fragments/LoginForm.jsx
@@ -9,20 +9,20 @@ function LoginForm() {
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
                             alt="logo" />
-                        Automotive Cashier
+                        Auto Center Parts
                     </a>
                     <div
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1
                                 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Sign in to your account
+                                Login Administrator 🧑🏻‍💻 
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label for="email"
-                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                        email</label>
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        username</label>
                                     <input type="email" name="email" id="email"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="name@company.com" required="" />
@@ -42,12 +42,11 @@ function LoginForm() {
                                                 required="" />
                                         </div>
                                         <div className="ml-3 text-sm">
-                                            <label for="remember" className="text-gray-500 dark:text-gray-300">Remember
-                                                me</label>
+                                            <label for="remember" className="text-gray-500 dark:text-gray-300">Ingat Saya</label>
                                         </div>
                                     </div>
                                     <a href="#"
-                                        className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
+                                        className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Lupa
                                         password?</a>
                                 </div>
                                 <Link to='/dashboard' >
@@ -55,11 +54,6 @@ function LoginForm() {
                                         className="w-full mt-4 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
                                         in</button>
                                 </Link>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#"
-                                        className="font-medium text-blue-600 hover:underline">Sign
-                                        up</a>
-                                </p>
                             </form>
                         </div>
                     </div>
