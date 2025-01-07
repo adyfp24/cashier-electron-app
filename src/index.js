1278666025aaeb5e3075502de0f3a8a62b509468
adyfp24 <adyfp24@gmail.com>
2025-01-07 15:35:00 +0700

    [FIX] app provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './app.jsx';
 import './index.css';
-import { AppProviders } from './provider/appProvider.jsx';
+import { AppProviders } from './provider/AppProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
