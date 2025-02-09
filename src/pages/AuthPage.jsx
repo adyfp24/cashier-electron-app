391aba4855572fc9abb4c5f0fe1cc42982f13b47
haris ardian <harisfirman387@gmail.com>
2025-02-09 09:14:20 +0700

    fix: auth handling

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -6,28 +6,28 @@ import { useNavigate } from 'react-router-dom'
 import { SettingContext } from '../context/settingContext'
 
 function AuthPage() {
-    const { isAuthenticated, login, error: authError, loading: authLoading } = useContext(AuthContext);
-    const { settings, loading: settingsLoading, error: settingsError } = useContext(SettingContext);
+    const {isAuthenticated, login, error: authError, loading: authLoading,user} = useContext(AuthContext);
+    const {settings, loading: settingsLoading, error: settingsError} = useContext(SettingContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (isAuthenticated) {
+        const token = localStorage.getItem('token');
+        if (token && isAuthenticated && user) {
             navigate('/dashboard');
         }
-    }, [isAuthenticated, navigate]);
+    }, [isAuthenticated, navigate, user]);
 
     if (authLoading || settingsLoading) {
         return (
-            <div>
+            <div className="flex items-center justify-center w-full h-screen">
                 <h1>Loading...</h1>
             </div>
         );
     }
 
-    if(isAuthenticated){
-        navigate('/dashboard')
+    if (isAuthenticated && user) {
+        return null;
     }
-
     console.log(settings)
     
     return (
