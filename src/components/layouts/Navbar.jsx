d3f3b2c6e11b2cc01528d50328f92a7b210210d2
haris ardian <harisfirman387@gmail.com>
2025-04-07 13:03:31 +0700

    merge development to build fix

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -26,6 +26,7 @@ function Navbar() {
                                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                                 </svg>
+                                
                             </button>
                             <a href="https://flowbite.com" class="flex ms-2 md:me-24">
                                 <img src={settings.logo_aplikasi} class="h-8 me-3" alt="FlowBite Logo" />
@@ -37,7 +38,7 @@ function Navbar() {
                                 <div className='flex items-center'>
                                     <div className="mr-3 text-center text-black dark:text-white">
                                         <div className="flex items-center justify-center">
-                                            <p className='font-medium text-gray-700'>Version 1.0.0</p>
+                                            <p className='font-medium text-gray-700 dark:text-gray-200'>Version 1.0.0</p>
                                         </div>
                                     </div>
                                 </div>
