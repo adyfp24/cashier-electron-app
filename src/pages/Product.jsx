44f71cb1a396d68d484cedf96f8e4d8b1d8f67bb
adyfp24 <adyfp24@gmail.com>
2025-04-08 19:47:04 +0700

    [FEAT] clear local db & fill dummy data

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -13,19 +13,21 @@ function Product() {
         deleteProduct, updateProduct, pagination, getAllProduct, getAllCategories, } = useContext(ProductContext);
 
     const [dropdownOpen, setDropdownOpen] = useState(null);
+    const [dropdownDirection, setDropdownDirection] = useState({});
     const [searchQuery, setSearchQuery] = useState('');
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [isFilterDropdownOpen, setIsFilterDropdownOpen] = useState(false);
-    
+
     const [isModalOpen, setModalOpen] = useState(false);
     const [isModalCategoryOpen, setModalCategoryOpen] = useState(false);
-    
+
     const [isCommonToastOpen, setIsCommonToastOpen] = useState(false);
-    const [toastMessage, setToastMessage] = useState({ 
-        type: 'success', 
-        message: '' });
-    
+    const [toastMessage, setToastMessage] = useState({
+        type: 'success',
+        message: ''
+    });
+
     const [selectedProduct, setSelectedProduct] = useState(null);
     const navigate = useNavigate()
 
@@ -101,13 +103,25 @@ function Product() {
         }
     }
 
-    const toggleDropdown = (productId) => {
+    const toggleDropdown = (productId, event) => {
         if (productId === dropdownOpen) {
             setDropdownOpen(null);
         } else {
             setDropdownOpen(productId);
-        }
 
+            // Calculate whether dropdown should open upward
+            if (event) {
+                const buttonPosition = event.target.getBoundingClientRect();
+                const viewportHeight = window.innerHeight;
+                const spaceBelow = viewportHeight - buttonPosition.bottom;
+
+                // If less than 100px below, open upward
+                setDropdownDirection(prev => ({
+                    ...prev,
+                    [productId]: spaceBelow < 100
+                }));
+            }
+        }
     };
 
     const toggleFilterDropdown = () => {
@@ -115,12 +129,12 @@ function Product() {
     };
 
     const handleCategoryFilter = (category) => {
-        setSelectedCategories(prev => 
+        setSelectedCategories(prev =>
             prev.includes(category) ? prev.filter(c => c !== category) : [...prev, category]
         );
     };
 
-    
+
     const clearFilter = () => {
         setSelectedCategories([]);
     };
@@ -141,7 +155,7 @@ function Product() {
         }
 
         if (selectedCategories.length > 0) {
-            filtered = filtered.filter(product => 
+            filtered = filtered.filter(product =>
                 selectedCategories.includes(product.jenisProduk)
             );
         }
@@ -149,7 +163,7 @@ function Product() {
         setFilteredProducts(filtered);
         console.log('Filtered Products:', filtered);
     }, [searchQuery, selectedCategories, products]);
-    
+
 
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
@@ -172,9 +186,9 @@ function Product() {
                 type={toastMessage.type}
             ></CommonToast>
 
-            <section className="min-h-screen p-3 pt-20 bg-gray-50 dark:bg-gray-900 sm:p-5 md:ml-64">          
+            <section className="min-h-screen p-3 pt-20 bg-gray-50 dark:bg-gray-900 sm:p-5 md:ml-64">
                 <div className="max-w-screen-xl px-4 mx-auto lg:px-12">
-                    <div className="relative mt-20 overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                    <div className="relative mt-20 overflow-visible bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
                         <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
                             <div className="w-full md:w-1/2">
                                 <form className="flex items-center">
@@ -185,18 +199,18 @@ function Product() {
                                                 <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                                             </svg>
                                         </div>
-                                        <input 
-                                            onChange={handleSearchChange} 
-                                            type="text" 
-                                            className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
-                                            placeholder="Search" 
+                                        <input
+                                            onChange={handleSearchChange}
+                                            type="text"
+                                            className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                            placeholder="Search"
                                         />
                                     </div>
                                 </form>
                             </div>
                             <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-                                <button 
-                                    onClick={handleAddCategory} 
+                                <button
+                                    onClick={handleAddCategory}
                                     className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                 >
                                     <svg className="h-3.5 w-3.5 mr-2 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -205,46 +219,46 @@ function Product() {
                                     Tambah Kategori
                                 </button>
 
-                                <button 
+                                <button
                                     onClick={handleAddProduct}
                                     className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                 >
-                                     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                                        </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+                                    </svg>
                                     Tambah Produk
                                 </button>
                                 <div className="relative w-full md:w-auto">
-                                <button 
-                                    onClick={toggleFilterDropdown} 
-                                    className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                                >
-                                     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
+                                    <button
+                                        onClick={toggleFilterDropdown}
+                                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                                    </svg>
-                                    Filter Kategori
-                                </button>
-                                {isFilterDropdownOpen && (
-                                    <div className="absolute right-0 w-48 mt-2 pb-10 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-600 max-h-60 overflow-y-auto">
-                                        {categories.map(category => (
-                                            <label key={category} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700">
-                                                <input 
-                                                    type="checkbox" 
-                                                    className="mr-2"
-                                                    checked={selectedCategories.includes(category)}
-                                                    onChange={() => handleCategoryFilter(category)}
-                                                />
-                                                <span className="text-sm text-gray-700 dark:text-gray-300">{category}</span>
-                                            </label>
-                                        ))}
-                                        <button 
-                                            onClick={clearFilter} 
-                                            className="w-full py-2 text-sm font-medium text-center text-red-500 hover:bg-red-100 dark:hover:bg-red-700"
-                                        >
-                                            Hapus Filter
-                                        </button>
-                                    </div>
-                                )}
+                                        </svg>
+                                        Filter Kategori
+                                    </button>
+                                    {isFilterDropdownOpen && (
+                                        <div className="absolute right-0 w-48 mt-2 pb-10 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-600 max-h-60 overflow-y-auto">
+                                            {categories.map(category => (
+                                                <label key={category} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                    <input
+                                                        type="checkbox"
+                                                        className="mr-2"
+                                                        checked={selectedCategories.includes(category)}
+                                                        onChange={() => handleCategoryFilter(category)}
+                                                    />
+                                                    <span className="text-sm text-gray-700 dark:text-gray-300">{category}</span>
+                                                </label>
+                                            ))}
+                                            <button
+                                                onClick={clearFilter}
+                                                className="w-full py-2 text-sm font-medium text-center text-red-500 hover:bg-red-100 dark:hover:bg-red-700"
+                                            >
+                                                Hapus Filter
+                                            </button>
+                                        </div>
+                                    )}
                                 </div>
                             </div>
                         </div>
@@ -279,7 +293,7 @@ function Product() {
                                             <td className="px-4 py-3">
                                                 <div className="relative flex justify-end">
                                                     <button
-                                                        onClick={() => toggleDropdown(product.id)}
+                                                        onClick={(e) => toggleDropdown(product.id, e)}
                                                         className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                                                         type="button"
                                                     >
@@ -288,7 +302,7 @@ function Product() {
                                                         </svg>
                                                     </button>
                                                     {dropdownOpen === product.id && (
-                                                        <div className="absolute right-0 z-50 mt-1 bg-white divide-y divide-gray-100 rounded shadow w-28 top-full dark:bg-gray-700 dark:divide-gray-600">
+                                                        <div className={`absolute right-0 z-50 bg-white divide-y divide-gray-100 rounded shadow w-28 dark:bg-gray-700 dark:divide-gray-600 ${dropdownDirection[product.id] ? 'bottom-full mb-1' : 'top-full mt-1'}`}>
                                                             <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
                                                                 <li onClick={() => handleEditProduct(product)} className="px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                                                     Edit
@@ -306,12 +320,14 @@ function Product() {
                                 </tbody>
                             </table>
                         </div>
-                        <Pagination 
-                            limit={pagination.limit} 
-                            totalPage={pagination.totalPage} 
-                            page={pagination.page} 
-                            setPage={setPage}
-                        />
+                        <div className="relative">
+                            <Pagination
+                                limit={pagination.limit}
+                                totalPage={pagination.totalPage}
+                                page={pagination.page}
+                                setPage={setPage}
+                            />
+                        </div>
                     </div>
                 </div>
             </section>
